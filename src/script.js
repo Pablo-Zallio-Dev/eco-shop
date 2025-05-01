498c7a5ae9a4d40bf18890ea87cf2d6e92587819
Pablo Zallio Dev <pablozalliodev@gmail.com>
2025-05-01 17:05:27 +0200

    Fixing errors in mobile menu

--- src/script.js
+++ src/script.js
@@ -1,12 +1,17 @@
 const d = document,
-    $navMenu = d.querySelector(".nav__menu")
+    $navMenu = d.querySelector(".nav__menu"),
+    $imgActive = d.querySelector(".nav__img")
+    $imgClose = d.querySelector(".nav__img__close")
 
 d.addEventListener("click", (e) => {
    console.log(e.target.classList)
-   if(e.target.classList.contains("nav__img")) {
+   if(e.target.classList.contains("nav__img")){
     $navMenu.classList.toggle('nav__menu--active')
+    $imgActive.classList.toggle('nav__img--disabled')
+    $imgClose.classList.toggle('nav__img--disabled')
    } else {
-    $navMenu.classList.remove('nav__menu--active')
-
+    $navMenu.classList.toggle('nav__menu--active')
+    $imgActive.classList.toggle('nav__img--disabled')
+    $imgClose.classList.toggle('nav__img--disabled')
    }
 })
